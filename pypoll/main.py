3cd18dca3415425a579827072f29d922f86b91ea
Ben Pharn <benjaminpharn@gmail.com>
2020-06-26 23:42:06 -0700

    pypoll done

--- pypoll/main.py
+++ pypoll/main.py
@@ -1,18 +1,56 @@
 #* In this challenge, you are tasked with helping a small, rural town modernize its vote counting process.
 
-#* You will be give a set of poll data called [election_data.csv](PyPoll/Resources/election_data.csv). The dataset is composed of three columns: `Voter ID`, `County`, and `Candidate`. Your task is to create a Python script that analyzes the votes and calculates each of the following:
+#* You will be give a set of poll data called [election_data.csv](PyPoll/Resources/election_data.csv). 
+# The dataset is composed of three columns: `Voter ID`, `County`, and `Candidate`. Your task is to create 
+# a Python script that analyzes the votes and calculates each of the following:
 
-#  * The total number of votes cast
+import os
+import csv
 
-#  * A complete list of candidates who received votes
+csvpath = os.path.join('resources', 'election_data.csv')
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    csv_header = next(csvreader)
+    totalcount = 0
+    votedict = {}
+    votepercent = []
+    winvote = 0
+    i = 0
+
+    for row in csvreader:
+        #  * The total number of votes cast
+        totalcount += 1
+        #  * A complete list of candidates who received votes
+        if row[2] not in votedict:
+            votedict.update({row[2]:1})
+            #print(f'{votedict}')    
+        #  * The total number of votes each candidate won
+        else:
+            votedict[row[2]] += 1
+            #print(f'{votedict}') 
 
 #  * The percentage of votes each candidate won
-
-#  * The total number of votes each candidate won
-
+#print(f'{votedict}') 
+for key in votedict:
+    votepercent.append(votedict[key])
+#print(f'{votepercent}') 
+votepercent[:] = [round(100 * i / totalcount, 3) for i in votepercent]
+#print(f'{votepercent}') 
 #  * The winner of the election based on popular vote.
+winner = max(votedict, key=votedict.get)
+#print(f'{winner}') 
 
 #* As an example, your analysis should look similar to the one below:
+
+print("Election Results")
+print("-------------------")
+print(f"Total Votes: {totalcount}")
+print("-------------------")
+for key in votedict:
+    print(f"{key}: {votepercent[i]}% ({votedict[key]})")
+    i += 1
+print("-------------------")
+print(f"Winner: {winner}")
 
 #  ```text
 #  Election Results
@@ -29,3 +67,15 @@
 #  ```
 
 #* In addition, your final script should both print the analysis to the terminal and export a text file with the results.
+i = 0
+file1 = open("output.txt", "a")
+file1.write("Election Results\n")
+file1.write("-------------------\n")
+file1.write("Total Votes: %i\n" % (totalcount))
+file1.write("-------------------\n")
+for key in votedict:
+    file1.write("%s: %5.2f%% (%i)\n" % (key, votepercent[i], votedict[key]))
+    i += 1
+file1.write("-------------------\n")
+file1.write("Winner: %s\n" % (winner))
+file1.close()
